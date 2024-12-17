86b03097f414039b40e2a85a80a0055c25c40f22
Prodigy610 <145206367+Prodigy610@users.noreply.github.com>
2024-12-17 13:52:13 +0300

    Delete Deploy to GitHub directory

--- Deploy to GitHub/js/mainapp.js
+++
@@ -1,9 +0,0 @@
-
-const header = document.getElementById('header');
-        window.addEventListener('scroll', function() {
-            if (window.scrollY > 750) {
-                header.classList.add('scrolled');
-            } else {
-                header.classList.remove('scrolled');
-            }
-});
